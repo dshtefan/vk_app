3403f760016209075322552cfd48913aebc99496
Denis Shtefan <den.shtefan@gmail.com>
2019-06-15 15:45:54 +0300

    Updates

---
+++ precache-manifest.4dbcf87cba8a5ce1a675b58adee8cccd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk_app/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "1618c56f04de3ba2778b",
+    "url": "/vk_app/static/js/runtime~main.1618c56f.js"
+  },
+  {
+    "revision": "5cb82ae3370dd6269a14",
+    "url": "/vk_app/static/js/main.5cb82ae3.chunk.js"
+  },
+  {
+    "revision": "7b919d35e7d1d568b4f2",
+    "url": "/vk_app/static/js/2.7b919d35.chunk.js"
+  },
+  {
+    "revision": "5cb82ae3370dd6269a14",
+    "url": "/vk_app/static/css/main.372eb214.chunk.css"
+  },
+  {
+    "revision": "7b919d35e7d1d568b4f2",
+    "url": "/vk_app/static/css/2.259dfe01.chunk.css"
+  },
+  {
+    "revision": "c1a8565e0da1969523c0e35816cad36e",
+    "url": "/vk_app/index.html"
+  }
+];
