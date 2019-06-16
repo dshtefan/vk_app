99686d9778a5d63a68ef3aa89a6137281749117d
Denis Shtefan <den.shtefan@gmail.com>
2019-06-17 02:18:09 +0300

    Добавлена реализация сервера

---
+++ server/server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var db = require("./db.json");
+
+app.use(function (req, res, next) {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+   res.setHeader('Access-Control-Allow-Credentials', true);
+   next();
+});
+
+//Для нормальной работы парсера
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// adress: localhost:3012/getpoll
+//Возвращает опрос по id
+app.get('/getpoll', function(req, res){
+   var poll_id = req.query.id;
+   for(var i = 0; i !== db.length; i++){
+      if (db[i].id == poll_id){
+         res.send(db[i]);
+         return;
+      }
+   }
+   res.send({ok: false});
+});
+
+// adress: localhost:3012/addanswer
+//Имитирует отправку ответов в бд
+app.put('/addanswer', function(req, res){
+   console.log(req.body);
+   res.send({ok: true});
+});
+
+//Запуск сервера на 3012 порту
+app.listen(3012, function(){
+   console.log("Server started on 3012 port");
+});
